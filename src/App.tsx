d8efde1cfc3e4df21996ca3d273205f7768687ed
ClariceMaciel <claros.maciel@gmail.com>
2021-11-16 23:54:13 -0300

    commit

---
+++ src/App.tsx
@@ -0,0 +1,71 @@
+import { useState, useEffect} from 'react';
+import * as C from './App.styles';
+import { Item } from './types/Item';
+import { Category} from './types/Category';
+import { categories } from './data/categories';
+import { items } from './data/items';
+import { getCurrentMonth, filterListByMonth} from './helpers/dateFilter';
+import { TableArea } from './components/TableArea';
+import { InfoArea } from './components/InfoArea';
+import { InputArea } from './components/InputArea'; 
+
+const App = () => {
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
+
+  useEffect(()=>{
+    setFilteredList( filterListByMonth(list, currentMonth));
+  }, [list, currentMonth]);
+
+  useEffect(()=>{
+    let incomeCount = 0;
+    let expenseCount = 0;
+
+    for(let i in filteredList){
+      if(categories[filteredList[i].category].expense){ //se expense é verdadeiro na categoria
+        expenseCount += filteredList[i].value;
+      } else {
+        incomeCount += filteredList[i].value;
+      }
+    }
+
+    setIncome(incomeCount);
+    setExpense(expenseCount);
+  }, [filteredList]);
+
+  const handleMonthChange = (newMonth: string) => {
+    setCurrentMonth(newMonth);
+  }
+
+  const handleAddItem = (item: Item) => {
+    let newList = [...list]; //copia da lista
+    newList.push(item); // adicionar item na lista
+    setList(newList);
+  }
+
+  return (
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>Sistema financeiro</C.HeaderText>
+      </C.Header>
+      <C.Body>
+      <InfoArea 
+        currentMonth={currentMonth}
+        onMonthChange={handleMonthChange}    
+        income={income}
+        expense={expense}  
+      />
+
+      <InputArea onAdd={handleAddItem} />
+
+      <TableArea list={filteredList} />
+
+      </C.Body>
+    </C.Container>
+  );
+}
+
+export default App;
